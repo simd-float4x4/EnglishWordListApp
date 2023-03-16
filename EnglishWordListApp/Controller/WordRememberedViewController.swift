6e342126bca755377de7cbb059898eef7057901e
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-03-16 23:33:22 +0900

    成績照会画面： 基底Viewの実装

---
+++ EnglishWordListApp/Controller/WordRememberedViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class WordRememberedViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view = WordRememberedListView()
+    }
+}
