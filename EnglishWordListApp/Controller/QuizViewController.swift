44398eb479c976d5a70cb84632e8cb33099c82d3
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-03-19 03:34:21 +0900

    クイズ画面：　Viewの実装

---
+++ EnglishWordListApp/Controller/QuizViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func loadView() {
+        self.view = QuizView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
