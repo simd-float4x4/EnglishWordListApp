f37d4a72c5c3a0d9de03f1f44b94381440ac30e8
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-03-25 20:46:59 +0900

    単語登録画面：　登録ボタンを押下時にダミーデータを登録する処理（残件リストへ）

--- EnglishWordListApp/Controller/AddWordViewController.swift
+++ EnglishWordListApp/Controller/AddWordViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class AddWordViewController: UIViewController {
+class AddWordViewController: UIViewController, UIGestureRecognizerDelegate {
 
     let sectionTitleArray = ["単語", "意味", "カテゴリ", "例文", "例文(日本語訳)"]
     
@@ -16,21 +16,32 @@ class AddWordViewController: UIViewController {
     }
 
     override func loadView() {
-        self.view = AddWordView()
+        super.loadView()
+        let wordView = AddWordView()
+        self.view = wordView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.myModel = EnglishWordsModel()
         settingTable()
     }
+    
     
     private func settingTable(){
         let view = self.view as! AddWordView
         view.tableView.delegate = self
         view.tableView.dataSource = self
+        view.tableView.tableFooterView = UIView()
         view.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        view.controller = self
     }
     
+    
+    ///
+    /// Notificationに変更を送り、tableViewを更新するファンクション
+    ///
     @objc func registerModel(){
         guard let model = myModel else { return }
         
@@ -40,31 +51,40 @@ class AddWordViewController: UIViewController {
                                             using: {
             [unowned self] notification in
             let wordView = self.view as! WordView
-             
+            
             wordView.tableView.reloadData()
          })
     }
     
-    @objc func registerWordButton() {
+    
+    ///
+    /// 単語を登録する
+    ///
+    @IBAction func registerWord() {
         myModel?.addWordList(
             registeredWord: Word.init(
                 id: 99999,
-                word: word,
-                meaning: meaning,
-                exampleSentence: exampleSentence,
-                exampleTranslation: exampleTranslation,
+                word: "affiliate",
+                meaning: "〜を連携させる",
+                exampleSentence: "In the States, doctors have their own offices and most are affiliated with a hospital within the area.",
+                exampleTranslation: "アメリカの医者は自分の診療所を持っていても、大抵は地域のどこかの病院に所属しています。",
                 wordCategory: [],
                 level: 0,
                 isSolved: false,
-                isSoftDeleted: false))}
+                isSoftDeleted: false))
+        registerModel()
+    }
+    
+    func addWord() {
+        // 単語を追加する処理を実行する
+        // TO-DO: Viewのボタンが押下できない不具合の修正
+        print("Add word")
+    }
 }
 
 extension AddWordViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        if indexPath.row % 2 != 0 {
-           
-        }
         return cell
     }
     
@@ -83,8 +103,17 @@ extension AddWordViewController: UITableViewDelegate {
      
     func tableView(_ table: UITableView,
                        numberOfRowsInSection section: Int) -> Int {
-        // TO-DO: Categoryだけ動的に変更できるようにする
-        return 2
+        // TO-DO: if文を使い、Categoryだけ動的に変更できるようにする
+        return 1
+    }
+    
+    
+    ///
+    /// TableViewが選択された時の挙動
+    ///
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }
+
--- EnglishWordListApp/View/AddWordView.swift
+++ EnglishWordListApp/View/AddWordView.swift
@@ -3,10 +3,19 @@ import UIKit
 class AddWordView: UIView {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var registerButton: UIButton!
+    
+    weak var controller: AddWordViewController?
+    
+    @IBAction func tappedAddButton(_ sender: UIButton) {
+        // Controllerのメソッドを呼び出す
+        controller?.addWord()
+    }
     
     override init(frame: CGRect){
         super.init(frame: frame)
         loadNib()
+        print("a")
     }
 
     required init(coder aDecoder: NSCoder) {
@@ -18,6 +27,7 @@ class AddWordView: UIView {
         let view = Bundle.main.loadNibNamed("AddWordView", owner: self, options: nil)?.first as! UIView
         view.frame = self.bounds
         self.addSubview(view)
+        print("c")
     }
 
 }
